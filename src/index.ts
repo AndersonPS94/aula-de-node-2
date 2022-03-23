581d5974f69322a1e786ec83e6ee03ef3e6ce9fd
AndersonPS94 <dson_adn@hotmail.com>
2022-03-23 18:47:43 -0300

    git push

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 
-import express from 'express';
+import express,{ Request, Response, NextFunction } from 'express';
 
 const app = express();
 
-app.get('/status', (req, res, next) => {
-    
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'bar'});
 });
+
+
+app.listen(3000, () => {
+    console.log('Aplicacao executando na porta 3000!');
+})
